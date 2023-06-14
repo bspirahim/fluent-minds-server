df853e483b7eca3262b5f105b3aab909002dc4b0
Abdur-P-Rahim <bspirahim@gmail.com>
2023-06-14 15:11:31 +0600

    get single class by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -36,6 +36,13 @@ async function run() {
    app.get('/classes', async (req, res) => {
     const result = await classCollection.find().toArray();
     res.send(result);
+  })
+
+  app.get('/classes/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await classCollection.findOne(query)
+    res.send(result)
   })
 
 
