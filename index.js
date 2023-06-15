ab47c91af3fa5868ec7904c73c65394c30e71364
Abdur-P-Rahim <bspirahim@gmail.com>
2023-06-15 12:51:10 +0600

    jwt token and manage user done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require("jsonwebtoken")
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -92,6 +93,8 @@ async function run() {
 
     });
 
+
+    /* class update api */
     app.put('/classes/:id', async(req, res)=>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
@@ -109,6 +112,7 @@ async function run() {
       res.send(result)
     })
 
+    /* class delete api */
     app.delete('/classes/:id', async(req, res) =>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
